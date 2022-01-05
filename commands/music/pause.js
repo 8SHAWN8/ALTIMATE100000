7823e4dc916a3d1a2cc11e4f0e7672e3364569fe
Shahinuzzaman Shawn <90779538+8SHAWN8@users.noreply.github.com>
2022-01-05 16:24:58 +0600

    Update pause.js

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -10,19 +10,78 @@ module.exports = {
         const serverQueue = ops.queue.get(message.guild.id);
         const { channel } = message.member.voice;
       try {
-        if (!channel) return message.channel.send('${prefix}I\'m sorry but you need to be in a voice channel to pause music!');
-        // if (message.guild.me.voice.channel !== message.member.voice.channel) {
-        //     return message.channel.send(" You Have To Be In The Same Channel With The Bot! ");
-        // };
-        // if (serverQueue && serverQueue.playing) {
-        //     serverQueue.playing = false;
-        //     serverQueue.connection.dispatcher.pause(true);
-        //     return message.channel.send(' Paused  ⏸');
-        // }
-        return message.channel.send('${prefix} There is Nothing Playing! ');
+        if (!channel) return message.channel.send('Hello how are you :) ?');
+        
+        return message.channel.send('hello kemon asen?');
       } catch {
           serverQueue.connection.dispatcher.end();
           await channel.leave();
       }
     }
 };
+
+module.exports = {
+
+        name: 'fine',
+        noalias: 'No Aliases',
+        category: "music",
+        description: 'Pause command.',
+        usage: " ",
+        accessableby: "everyone",
+    run: async (bot, message, args, ops) => {
+        const serverQueue = ops.queue.get(message.guild.id);
+        const { channel } = message.member.voice;
+      try {
+        if (!channel) return message.channel.send('ok?');
+        
+        return message.channel.send('oook :)');
+      } catch {
+          serverQueue.connection.dispatcher.end();
+          await channel.leave();
+      }
+    }
+};
+module.exports = {
+
+        name: 'good',
+        noalias: 'No Aliases',
+        category: "music",
+        description: 'Pause command.',
+        usage: " ",
+        accessableby: "everyone",
+    run: async (bot, message, args, ops) => {
+        const serverQueue = ops.queue.get(message.guild.id);
+        const { channel } = message.member.voice;
+      try {
+        if (!channel) return message.channel.send('oh, nice');
+        
+        return message.channel.send('hmm');
+      } catch {
+          serverQueue.connection.dispatcher.end();
+          await channel.leave();
+      }
+    }
+};
+
+module.exports = {
+
+        name: '@!   ToXicツ EvAn 👑',
+        noalias: 'No Aliases',
+        category: "music",
+        description: 'Pause command.',
+        usage: " ",
+        accessableby: "everyone",
+    run: async (bot, message, args, ops) => {
+        const serverQueue = ops.queue.get(message.guild.id);
+        const { channel } = message.member.voice;
+      try {
+        if (!channel) return message.channel.send('I think he is not online now ,you can dm him');
+        
+        return message.channel.send('I think he is not online now you can dm him ');
+      } catch {
+          serverQueue.connection.dispatcher.end();
+          await channel.leave();
+      }
+    }
+};
+
